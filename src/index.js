ec8ed8266ab4906cacf85ee24d36c8449c74d6ec
Wulfes <wolvenhollow@gmail.com>
2022-01-11 00:37:45 -0600

    Parity with Codepen

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ const title = document.createElement('div');
 const contentWrapper = document.createElement('div');
 const navWrapper = document.createElement('div');
 const listWrapper = document.createElement('div');
+const newTaskScreen = document.getElementById('newTaskScreen');
+
+const taskSubmitBtn = document.getElementById('taskSubmitButton');
+taskSubmitBtn.addEventListener('click', (x) => {createNewTask()});
+
+const newTaskName = document.getElementById('name');
+const newTaskDescription = document.getElementById('description');
+const newTaskDate = document.getElementById('date');
+const newTaskNotes = document.getElementById('notes');
+const newTaskPriority = document.getElementById('priority');
+
+const projectWrapper = setupNav();
 
 page.classList.add('page');
 title.classList.add('title');
@@ -15,18 +27,22 @@ contentWrapper.classList.add('contentWrapper');
 navWrapper.classList.add('navWrapper');
 listWrapper.classList.add('listWrapper');
 
-page.appendChild(title);
-page.appendChild(contentWrapper);
-contentWrapper.append(navWrapper);
-contentWrapper.append(listWrapper);
+page.append(newTaskScreen, title, contentWrapper);
+contentWrapper.append(navWrapper, listWrapper);
 
 title.textContent = 'To Do List';
 
-class Project{
-    constructor(name, description, dueDate, priority){
+let defaultProject = [];
+
+let projectArray = [defaultProject];
+let currentProject = [defaultProject];
+
+class Task{
+    constructor(name, description, dueDate, notes, priority){
         this.name = name;
         this.description = description;
         this.dueDate = dueDate;
+        this.notes = notes;
         this.priority = priority;
     }
 
@@ -69,25 +85,71 @@ function setupNav(){
 
     projectWrapper.textContent = 'Projects';
 
-    const defaultProject = new Project('Default', 'Default Project', null,null);
-    const nextProject = new Project('nextProj', `it's the next project`, null, null);
+    // const defaultProject = new Project('Default', 'Default Project', null,null);
+    // const nextProject = new Project('nextProj', `it's the next project`, null, null);
     
-    let projectArray = [defaultProject];    
-    defaultProject.projectNode = createClassDiv(defaultProject);
-    nextProject.projectNode = createClassDiv(nextProject);
-    listWrapper.appendChild(defaultProject.projectNode);
-    listWrapper.appendChild(nextProject.projectNode);
+    projectWrapper.textContent = 'Projects';    
+    const addNewTaskButton = document.createElement('div');
+    addNewTaskButton.classList.add('addTask');
+    addNewTaskButton.addEventListener('click', (x)=>{addNewTask(x)});
+  
+    projectWrapper.appendChild(addNewTaskButton);  
+    return projectWrapper;
 
-    const defaultDiv = document.createElement('div');
-    defaultDiv.classList.add('navButton');
-    defaultDiv.textContent = 'Default';
-    defaultDiv.addEventListener('click', ()=>{changePage(defaultDiv)});
-    
-    projectWrapper.appendChild(defaultDiv);
 }
-
-setupNav();
 
 function changePage(node){
     console.log('hello this works');
+}
+
+function addNewTaskDiv(classObject){
+    let newDivWrapper = document.createElement('div');
+    let nameDiv = document.createElement('div');
+    let descriptionDiv = document.createElement('div');
+    let dueDateDiv = document.createElement('div');
+    let priorityDiv = document.createElement('div');
+
+    nameDiv.textContent = classObject.name;
+    descriptionDiv.textContent = classObject.description;
+    dueDateDiv.textContent = classObject.dueDate;
+    priorityDiv.textContent = classObject.priority;
+
+    newDivWrapper.classList.add('divWrapper');
+    nameDiv.classList.add('nameDiv');
+    descriptionDiv.classList.add('descriptionDiv');
+    dueDateDiv.classList.add('dueDateDiv');
+    priorityDiv.classList.add('priorityDiv');
+ 
+    newDivWrapper.append(nameDiv);
+    newDivWrapper.append(descriptionDiv);
+    newDivWrapper.append(dueDateDiv);
+    newDivWrapper.append(priorityDiv);
+  
+    classObject.projectNode = newDivWrapper;
+
+    return newDivWrapper;
+}
+
+function addNewTask(){
+    let classTask = newTaskScreen.classList;
+    if (classTask.contains('show')){  
+      newTaskScreen.classList.remove('show');
+    }
+    else {newTaskScreen.classList.add('show');}
+  }
+  
+function addProjectNav(target){
+    let newProjectInput = document.createElement('input');
+    projectWrapper.appendChild(newProjectInput);
+}
+
+function createNewTask(){
+    let newTask = new Task(newTaskName.value, 
+                                newTaskDescription.value,
+                                newTaskDate.value,
+                                newTaskNotes.value,
+                                newTaskPriority.value);
+    let newDiv = addNewTaskDiv(newTask)
+    currentProject.push(newDiv);
+    listWrapper.appendChild(newDiv);
 }
